de137dd042c77aa59cceb66842b3c005359e9c1a
Noah Campise <noahcampise@Noahs-MacBook-Pro.local>
2026-03-14 01:16:19 -0500

    title

---
+++ pause_menu/pause_menu_start.gd
@@ -0,0 +1,32 @@
+extends Control
+@onready var optionsMenu = preload("res://pause_menu/pause_menu.tscn")
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	self.visible = false
+
+func pause():
+	self.visible = true
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+func testEsc():
+	if Input.is_action_just_pressed("escape_press") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("escape_press") and get_tree().paused: 
+		resume()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _process(delta: float) -> void:
+	testEsc()
